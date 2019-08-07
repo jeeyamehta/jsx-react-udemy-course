c2d20d940567a0e8695a49185bdb4fcfa8fc03bd
Jeeya Mehta <jmgrd-adm@yahoo.com>
2019-08-07 13:24:16 -0400

    jsx

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a React component
-const App = function() {
-    return <div>Hi there!</div>;
+const App = () => {
+    const buttonText = {text: 'Click Me'};
+    const labelText = 'Enter Name:';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
 };
 
 // Take the React component and show it on the screen
@@ -12,3 +25,10 @@ ReactDOM.render(
     <App />,
     document.querySelector('#root')
 );
+
+
+
+
+
+
+
